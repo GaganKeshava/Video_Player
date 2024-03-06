778414c34cca7884ca96f8324e6e971799f9c175
Gagan K <gagan.innov.con@phonepe.com>
2024-03-06 21:08:43 +0530

    changed play/pause text to icon

--- src/scripts/VideoPlayer.jsx
+++ src/scripts/VideoPlayer.jsx
@@ -78,12 +78,35 @@ const VideoPlayer = ({ autoplay }) => {
           height="100%"
         />
         <div className="flex text-xs justify-between items-center mt-2 px-2">
-          <div>
-            <button
-              className="bg-slate-300 rounded-md p-1 mr-2"
-              onClick={handlePlayPause}
-            >
-              Play/Pause
+          <div className="flex items-center">
+            <button className="rounded-md p-1 mr-2" onClick={handlePlayPause}>
+              {videoRef.current?.paused ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M6.75 5.25a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V5.25Zm7.5 0A.75.75 0 0 1 15 4.5h1.5a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H15a.75.75 0 0 1-.75-.75V5.25Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              )}
             </button>
             {displayTime} / {duration}
           </div>
